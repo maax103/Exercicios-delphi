5a77b078c5fe8cd743f997f3107f1e03a86792e8
FURB\alinescholl <aline.scholl13@gmail.com>
2022-10-04 20:03:43 -0300

    alterações atividade 35

--- Ex28/Uex28.pas
+++ Ex28/Uex28.pas
@@ -45,53 +45,6 @@ begin
   escreverResposta;
 end;
 
-procedure TfrmEx28.cadastrarFuncionarios(const aQtdFuncionarios : Integer);
-var
-  xNome : String;
-  xSalario : Currency;
-  xNumeroFunc, I : Integer;
-  begin
-  I := 0;
-
-  repeat
-    xNumeroFunc := Length(FFuncionarios) + 1;
-    SetLength(FFuncionarios, xNumeroFunc);
-
-    xNome := InputBox(
-      Format('Salário do funcionário %d',[xNumeroFunc]),
-      'Nome','');
-    xSalario := StrToCurr(InputBox(
-      Format('Salário do funcionário %d',[xNumeroFunc]),
-      'Salário',''));
-
-    FFuncionarios[xNumeroFunc - 1].nome := xNome;
-    FFuncionarios[xNumeroFunc - 1].salario := xSalario;
-
-    Inc(I)
-  until (I < aQtdFuncionarios);
-end;
-
-procedure TfrmEx28.calcularNovoSalario;
-var
-  xSalario : Currency;
-  xAjustePerc : Double;
-  I : Integer;
-const SALARIO_MINIMO = 1212;
-begin
-  for I := 0 to Length(FFuncionarios) - 1 do
-  begin
-    xSalario := FFuncionarios[I].salario;
-    if xSalario < 3 * SALARIO_MINIMO then
-      xAjustePerc := 1.5
-    else if xSalario <= 10 * SALARIO_MINIMO then
-      // Continuar daqui
-
-
-    FFuncionarios[I].ultimoSalario := xSalario;
-    FFuncionarios[I].salario := FFuncionarios[I].salario * xAjustePerc;
-  end;
-end;
-
 procedure TfrmEx28.escreverResposta;
 var
   I : Integer;
